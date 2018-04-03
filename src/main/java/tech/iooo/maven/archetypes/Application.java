c506c2443ac79ddcbc982c734ae2b54af779b129
YangkiZhang <yangkizhang@gmail.com>
2018-04-03 11:22:53 +0800

    [REFACTOR]:[first commit]
    Signed-off-by: YangkiZhang <yangkizhang@gmail.com>

---
+++ src/main/java/tech/iooo/maven/archetypes/Application.java
@@ -0,0 +1,16 @@
+package tech.iooo.maven.archetypes;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Hello world!
+ */
+public class Application {
+
+  private static final Logger logger = LoggerFactory.getLogger(Application.class);
+
+  public static void main(String[] args) {
+    logger.info("Hello World!");
+  }
+}
